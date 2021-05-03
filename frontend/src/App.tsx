7383444d53f159bf54ec3d6752dfc65cc6c6fb09
Patrick Moura <patrickmoura@gmail.com>
2021-05-03 19:26:45 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import NavBar from "components/NavBar/index";
+import Footer from "components/Footer/indesx";
+import DataTable from "components/DataTable";
 
 function App() {
   return (
-    <div >
-      <h1 className = "text-primary">Olá Mundo</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className = "container" >
+        <h1 className="text-primary">Olá Mundo</h1>
+        <DataTable/>
+      </div>
+      <Footer/>
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/indesx.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/patrickmouradev" target="_blank" rel="noreferrer">Patrick Moura</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@d evsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
+
+
+
+
+
