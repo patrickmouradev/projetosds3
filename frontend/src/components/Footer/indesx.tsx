a46bf8f051d33065fc8d5dc835df24e2406dd0dd
Patrick Moura <patrickmoura@gmail.com>
2021-05-07 20:46:57 -0300

    corrigindo espaçamento no footer

--- frontend/src/components/Footer/indesx.tsx
+++ frontend/src/components/Footer/indesx.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/patrickmouradev" target="_blank" rel="noreferrer">Patrick Moura</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
-      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@d evsuperior.ig</a></small></p>
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
         </footer>
     );
