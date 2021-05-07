037d22815d732660377136ebf1db615ccdccda10
Patrick Moura <patrickmoura@gmail.com>
2021-05-07 19:14:19 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,14 @@
+import { type } from "node:os"
+
 export type SaleSum =     {
     sellerName: string,
     sum: number
 }
+
+
+export type SaleSuccess = 
+    {
+        sellerName: string,
+        visited: number,
+        deals: number
+    }
